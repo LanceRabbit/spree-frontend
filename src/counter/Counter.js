68e1232386059292c6f377d2377660f5103c3be6
LanceRabbit <lancerabbit@gmail.com>
2020-10-28 18:35:33 +0800

    dispatch method

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,12 +1,20 @@
 import { React } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment } from './redux';
 
 
 const Counter = () => {
   const count = useSelector(s => s.counter.count);
+  const dispatch = useDispatch();
   return (
     <div>
       count: { count }
+      <br/>
+      <button onClick={() => {
+        dispatch(increment())
+      }}>
+        +1
+      </button>
     </div>
   )
 }
